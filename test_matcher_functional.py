54789b7c04bfb4f89efe3f4b799e04cec7bced39
will-s-hart <william.hart@maths.ox.ac.uk>
2025-12-22 19:02:46 +0000

    Add test cases

--- test_matcher_functional.py
+++ test_matcher_functional.py
@@ -12,6 +12,16 @@ from matcher import find_piece_in_template
         ("piece_1.jpg", 0, 0, 90, 7, 25),
         ("piece_2.jpg", 2, 2, 0, 11, 20),
         ("piece_3.jpg", 2, 2, 90, 15, 12),
+        ("piece_4.jpg", 1, 1, 0, 13, 27),
+        ("piece_5.jpg", 0, 2, 180, 11, 6),
+        ("piece_6.jpg", 1, 2, 270, 18, 20),
+        ("piece_7.jpg", 2, 0, 270, 7, 13),
+        ("piece_8.jpg", 0, 2, 270, 18, 24),
+        ("piece_10.jpg", 0, 2, 90, 2, 5),
+        ("piece_11.jpg", 1, 1, 180, 27, 8),
+        ("piece_12.jpg", 1, 1, 0, 18, 21),
+        ("piece_13.jpg", 2, 0, 90, 2, 4),
+        ("piece_14.jpg", 0, 2, 180, 25, 10),
     ],
 )
 def test_find_piece_expected_location(
